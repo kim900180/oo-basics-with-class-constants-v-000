1b1fc45aeb15b9c8a7650d7f971604d052697bc6
Jae Yong Kim <kim900180@gmail.com>
2018-08-01 02:00:53 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,14 +6,10 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    i = 0
-    while i < BRANDS.length
-      BRANDS.each do |shoe_brand|
-        if BRANDS[i] != shoe_brand
-          BRANDS << brand
-        end
-        i += 1
-      end
+    if BRANDS.any? {|shoe_brand| shoe_brand == brand}
+      BRANDS
+    else
+      BRANDS << brand
     end
   end
 
